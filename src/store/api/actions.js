e7bb1c52b9568073ac7ded509c3c18e903c305ab
Diego Guzmán <guzmand.dag@gmail.com>
2020-07-20 00:06:59 +0200

    Basic user page functionality

--- src/store/api/actions.js
+++ src/store/api/actions.js
@@ -2,11 +2,13 @@ import axios from 'axios'
 import { SET_PAGES,
   SET_TOTAL_PAGES,
   SET_LAST_FETCHED_PAGE,
+  SET_DISPLAYED_USER,
 } from './mutations'
 const api = process.env.VUE_APP_API_ENDPOINT
 
 
 export const getNextPage = 'getNextPage'
+export const getUser = 'getUser'
 
 export const actions = {
 
@@ -34,5 +36,21 @@ export const actions = {
     commit(SET_LAST_FETCHED_PAGE, pageNumber)
   },
 
+  async [getUser]({ commit }, { id, user }) {
+    if (user) {
+      return commit(SET_DISPLAYED_USER, user)
+    }
+    commit(SET_DISPLAYED_USER, null)
+    let response
+    try {
+      response = (await axios(`${api}/users/${id}`))?.data?.data
+    } catch (err) {
+      console.error(err)
+    }
+    if (!response) {
+      return
+    }
+    return commit(SET_DISPLAYED_USER, response)
+  },
 
 }
---
+++ src/views/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        v-if="user"
+        class="user"
+    >
+        <h1 v-text="fullName" />
+        <img
+            :src="user.avatar"
+            :alt="fullName"
+        >
+        <a 
+            :href="`mailto:${user.email}`"
+        >email: {{ user.email }}</a>
+        {{ user }}
+    </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      user: state=> state.api.displayedUser,
+    }),
+    fullName() {
+      return `${this.user.first_name} ${this.user.last_name}`
+    },
+  },
+}
+</script>
